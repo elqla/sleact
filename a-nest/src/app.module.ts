69e0a5f42b48f9adc48727a3bd9a8eb2156bdbbc
dabeen <97648343+elqla@users.noreply.github.com>
2024-02-16 20:13:45 +0900

    envFileSetting

--- a-nest/src/app.module.ts
+++ a-nest/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+const getEnv = async () => {
+  // axios.get(/비밀키 요청)
+  return {
+    DB_PASSWORD: 'nodejsbook',
+    NAME: '제로초바보',
+  };
+};
 @Module({
-  imports: [],
+  // forRoot등 안에 설정을 넣어주어야함.
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
