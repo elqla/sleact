501d677295d41f198f3a88f58b5641b19ee457f6
dabeen <97648343+elqla@users.noreply.github.com>
2024-02-16 19:09:01 +0900

    init

--- back/passport/index.js
+++ back/passport/index.js
@@ -4,12 +4,14 @@ const User = require("../models/user");
 const Workspace = require("../models/workspace");
 
 module.exports = () => {
+  // 쿠키에 id 만 저장하기
   passport.serializeUser((user, done) => {
     // 서버쪽에 [{ id: 1, cookie: 'clhxy' }]
     done(null, user.id);
   });
 
   passport.deserializeUser(async (id, done) => {
+    // 매 요청마다, 디비에서 사용자 정보 가져옴
     try {
       const user = await User.findOne({
         where: { id },
