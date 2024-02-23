05db78230165cd2ef4644538dddde409e5ff5cf5
dabeen <97648343+elqla@users.noreply.github.com>
2024-02-23 20:43:35 +0900

    queryRunner

--- a-nest/src/users/users.service.ts
+++ a-nest/src/users/users.service.ts
@@ -6,8 +6,10 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import bcrypt from 'bcrypt';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
 
 @Injectable()
 export class UsersService {
@@ -16,10 +18,24 @@ export class UsersService {
     // Dependency injection시 실제 객체는 module에서 넣어줘야함.
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    // 요기 추가하면 module에 import 추가하는거 잊지말기
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    // transaction
+    private dataSource: DataSource,
   ) {}
 
   // async에서 throw한 경우엔 서버를 멈추지 않고, 경고만 뜨고 만다.
   async join(email: string, nickname: string, password: string) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    // app.module에 import한 typeormmodule.forroot를 통해 실행될지
+    // 현재 connect를 통해 실행될까?
+    // this를 통해 가져온 레포지토리는 transactionconnect이 아닌 처음 실행된 connection에 맺어지므로
+    // (queryrunner를 통해 가져와야한다..)
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
     if (!nickname) {
       throw new BadRequestException('nickname이 없어요.');
     }
@@ -30,7 +46,11 @@ export class UsersService {
       throw new BadRequestException('password가 없어요.');
     }
 
-    const user = await this.usersRepository.findOne({ where: { email } });
+    const user = await queryRunner.manager
+      .getRepository(Users)
+      .findOne({ where: { email } });
+
+    // const user = await this.usersRepository.findOne({ where: { email } });
 
     if (user) {
       // 이미 존재
@@ -38,10 +58,35 @@ export class UsersService {
       throw new UnauthorizedException('이미 존재하는 사용자입니다.');
     }
     const hashedPassword = await bcrypt.hash(password, 12);
-    await this.usersRepository.save({
-      email,
-      nickname,
-      password: hashedPassword,
-    });
+    // save = insert into
+    try {
+      const returned = await queryRunner.manager.getRepository(Users).save({
+        email,
+        nickname,
+        password: hashedPassword,
+      });
+      // 저장한 유저값 = returned
+      await queryRunner.manager.getRepository(WorkspaceMembers).save({
+        UserId: returned.id,
+        WorkspaceId: 1,
+      });
+      // 이렇게도 쓸 수 있음.
+      // const workspaceMember = new WorkspaceMembers(); // this.workspaceMembersRepository.create()도 가능
+      // workspaceMember.UserId = returned.id;
+      // workspaceMember.WorkspaceId = returned.id;
+      // await this.workspaceMembersRepository.save(workspaceMember)
+      await queryRunner.manager.getRepository(ChannelMembers).save({
+        UserId: returned.id,
+        ChannelId: 1,
+      });
+      return true;
+    } catch (error) {
+      console.error(error);
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      // db에 최대 연결 개수가 있어서, 수동 커넥트, 릴리즈 필수
+      await queryRunner.release();
+    }
   }
 }
