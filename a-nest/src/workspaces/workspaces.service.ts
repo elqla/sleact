e3e05d7cc01f62d71deb02bbff58cf436584847b
dabeen <97648343+elqla@users.noreply.github.com>
2024-02-24 16:27:08 +0900

    typeorm querybuilder

--- a-nest/src/workspaces/workspaces.service.ts
+++ a-nest/src/workspaces/workspaces.service.ts
@@ -35,6 +35,16 @@ export class WorkspacesService {
   }
 
   async createWorkspace(name: string, url: string, myId: number) {
+    // 워크스페이스 만들기
+    // entity 객체 만들고 save create 헷갈려서 잘 안씀.
+    // const workspace = this.workspacesRepository.create({
+    //   name,
+    //   url,
+    //   OwnerId: myId,
+    // });
+    // const returned = this.workspacesRepository.save(workspace);
+
+    // Promiseall 로 시간 단축
     const workspace = new Workspaces();
     workspace.name = name;
     workspace.url = url;
@@ -54,14 +64,23 @@ export class WorkspacesService {
     await this.channelMembersRepository.save(channelMember);
   }
 
+  // querybuilder - sql느낌
   async getWorkspaceMembers(url: string) {
     return this.usersRepository
       .createQueryBuilder('user')
-      .innerJoin('user.WorkspaceMembers', 'members')
+      .innerJoin('user.WorkspaceMembers', 'members') // entity에 대한 별명 'members'
       .innerJoin('members.Workspace', 'workspace', 'workspace.url = :url', {
         url,
       })
       .getMany();
+
+    // .innerJoin('u.Workspaces', 'm') // manytomany 버그때문에 위처럼 씀.
+
+    // getMany:  members. .앞을 객체로 인식해서 문자열로 만드는걸 피할 수 있음.
+    // getRawMany: Id, Pw, W.name, w.url
+    // {id: '', email:'', 'workspace.name':''}
+
+    // 성능 중요하면 rawquery쓰면 됨.
   }
 
   async createWorkspaceMembers(url, email) {
